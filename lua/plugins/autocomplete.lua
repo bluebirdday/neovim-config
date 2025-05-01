9b31f6556f7c24a1fb58c4c17cd1b4aa5b2f1877
Martin Dubbelman <martindubbelman@bluebirdday.nl>
2025-05-01 14:17:38 +0200

    Added autocomplete

--- lua/plugins/autocomplete.lua
+++ lua/plugins/autocomplete.lua
@@ -1,29 +1,76 @@
 return {
-   {
-           'williamboman/mason.nvim',
-           config = function() require('mason').setup() end,
-       },
-       {
-           'neovim/nvim-lspconfig',
-           dependencies = { 'williamboman/mason.nvim' },
-           config = function()
+    {
+        'williamboman/mason.nvim',
+        config = function()
+            require('mason').setup()
+        end,
+    },
+    {
+        'neovim/nvim-lspconfig',
+        dependencies = { 'williamboman/mason.nvim' },
+        config = function()
 
-           end
-       },
-       {
-           'williamboman/mason-lspconfig.nvim',
-           dependencies = { 'williamboman/mason.nvim', 'neovim/nvim-lspconfig' },
-           config = function()
-               local config = require('mason-lspconfig')
+        end
+    },
+    {
+        'williamboman/mason-lspconfig.nvim',
+        dependencies = { 'williamboman/mason.nvim', 'neovim/nvim-lspconfig' },
+        config = function()
+            local config = require('mason-lspconfig')
 
-               config.setup({
-                   ensure_installed = { 'lua_ls', 'eslint', 'phpactor' }
-               })
+            config.setup({
+                ensure_installed = { 'lua_ls', 'eslint', 'phpactor' }
+            })
 
-               config.setup_handlers({ function(server_name)
-                   require('lspconfig')[server_name].setup({})
-               end
-               })
-           end
-       },
+            config.setup_handlers({ function(server_name)
+                require('lspconfig')[server_name].setup({})
+            end
+            })
+        end
+    },
+    {
+        "L3MON4D3/LuaSnip",
+        keys = function()
+            return {}
+        end,
+    },
+    -- then: setup supertab in cmp
+    {
+        "hrsh7th/nvim-cmp",
+        dependencies = {
+            "hrsh7th/cmp-emoji",
+        },
+        config = function()
+            local cmp = require("cmp")
+            cmp.setup({
+                sources = {
+                    { name = "nvim_lsp" }
+                },
+
+                mapping = cmp.mapping.preset.insert({
+                    -- Navigate between completion items
+                    ['<C-p>'] = cmp.mapping.select_prev_item({ behavior = 'select' }),
+                    ['<C-n>'] = cmp.mapping.select_next_item({ behavior = 'select' }),
+
+                    -- `Enter` key to confirm completion
+                    ['<Tab>'] = cmp.mapping.confirm({ select = true }),
+
+                    -- Ctrl+Space to trigger completion menu
+                    ['<C-Space>'] = cmp.mapping.complete(),
+
+                    -- Scroll up and down in the completion documentation
+                    ['<C-u>'] = cmp.mapping.scroll_docs(-4),
+                    ['<C-d>'] = cmp.mapping.scroll_docs(4),
+                }),
+                snippet = {
+                    expand = function(args)
+                        vim.snippet.expand(args.body)
+                    end,
+                }
+            })
+        end,
+    },
+    {
+        'hrsh7th/cmp-nvim-lsp'
+    }
 }
