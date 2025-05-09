3717966d34e54c69ebcb7ccccf58ac927e514e60
Martin Dubbelman <martindubbelman@bluebirdday.nl>
2025-05-09 08:35:37 +0200

    Installed nvchad

--- README.md
+++ README.md
@@ -1,29 +1,9 @@
-# neovim configuration for Bluebird Day servers
+**This repo is supposed to used as config by NvChad users!**
 
-## Requirements
-neovim >= 0.7
-Not all Hypernodes have the same neovim version, this can be checked with:
-```nvim --version```
+- The main nvchad repo (NvChad/NvChad) is used as a plugin by this repo.
+- So you just import its modules , like `require "nvchad.options" , require "nvchad.mappings"`
+- So you can delete the .git from this repo ( when you clone it locally ) or fork it :)
 
-## Installation
-Install packer:
+# Credits
 
-```
-git clone --depth 1 https://github.com/wbthomason/packer.nvim ~/.local/share/nvim/site/pack/packer/start/packer.nvim
-```
-
-Clone the repo
-```
-git clone git@github.com:bluebirdday/neovim-config.git ~/.config/nvim
-```
-
-First time start will generate an error because of Packer missing modules, install these modules:
-```
-:PackerInstall
-```
-Quit and restart neovim
-
-## What does this do?
-
-- Uses Packer package manger as lazy does require neovim >= 0.8 and Hypernode has 0.7 installed.
-- Sets some keymappings and installs nord colorscheme
+1) Lazyvim starter https://github.com/LazyVim/starter as nvchad's starter was inspired by Lazyvim's . It made a lot of things easier!
--- init.lua
+++ init.lua
@@ -1,89 +1,37 @@
-require("plugins")
-
-vim.cmd[[colorscheme onenord]]
-vim.cmd[[set noshowmode]]
-
-vim.o.incsearch = true
-vim.o.hlsearch = true
-vim.o.ignorecase = true
-vim.o.smartcase = true
-vim.o.number = true
-vim.o.relativenumber = true
+vim.g.base46_cache = vim.fn.stdpath "data" .. "/base46/"
 vim.g.mapleader = " "
 
-vim.o.shiftwidth = 4
-vim.o.smartindent = true
+-- bootstrap lazy and all plugins
+local lazypath = vim.fn.stdpath "data" .. "/lazy/lazy.nvim"
 
-vim.o.softtabstop = 4
-vim.o.tabstop = 4
-vim.o.termguicolors = true
+if not vim.uv.fs_stat(lazypath) then
+  local repo = "https://github.com/folke/lazy.nvim.git"
+  vim.fn.system { "git", "clone", "--filter=blob:none", repo, "--branch=stable", lazypath }
+end
 
-vim.opt.cursorline = true
+vim.opt.rtp:prepend(lazypath)
 
--- Minimal number of screen lines to keep above and below the cursor.
-vim.opt.scrolloff = 10
+local lazy_config = require "configs.lazy"
 
+-- load plugins
+require("lazy").setup({
+  {
+    "NvChad/NvChad",
+    lazy = false,
+    branch = "v2.5",
+    import = "nvchad.plugins",
+  },
 
-vim.keymap.set('n', '<Esc>', '<cmd>nohlsearch<CR>')
-vim.opt.list = true
-vim.opt.listchars = { tab = '» ', trail = '·', nbsp = '␣' }
+  { import = "plugins" },
+}, lazy_config)
 
--- Open the explorer
-vim.keymap.set('n', '<leader>e', ':Explore<CR>')
+-- load theme
+dofile(vim.g.base46_cache .. "defaults")
+dofile(vim.g.base46_cache .. "statusline")
 
--- move line one up
-vim.keymap.set('n', '<leader>j', ':m +1<CR>')
-vim.keymap.set('n', '<leader>k', ':m -2<CR>')
+require "options"
+require "nvchad.autocmds"
 
-vim.keymap.set('n', 'dd', '"_dd')
-vim.keymap.set('n', 'x', '"_x')
-vim.keymap.set('n', 'd', '"_d')
-
-vim.keymap.set('n', '<C-d>', '<C-d>zz', opts)
-vim.keymap.set('n', '<C-u>', '<C-u>zz', opts)
-
--- find and center
-vim.keymap.set('n', 'n', 'nzzzv', opts)
-vim.keymap.set('n', 'N', 'Nzzzv', opts)
-
-vim.keymap.set('n', '<Tab>', ':bnext<CR>', opts)
-vim.keymap.set('n', '<S-Tab>', ':bprevious<CR>', opts)
-
-vim.keymap.set('n', '<leader>x', ':bdelete!<CR>', opts) --close buffer
-vim.keymap.set('n', '<leader>b', '<cmd> enew <CR>', opts) -- new buffer
-
-vim.keymap.set('n', '<leader>to', ':tabnew<CR>', opts) -- open new tab
-vim.keymap.set('n', '<leader>tx', ':tabclose<CR>', opts) --close current tab
-vim.keymap.set('n', '<leader>tn', ':tabn<CR>', opts) -- next tab
-vim.keymap.set('n', '<leader>tp', ':tabp<CR>', opts) -- previous tab
-
--- window split
-vim.keymap.set('n', '<leader>v', '<C-w>v', opts) -- vertical split
-vim.keymap.set('n', '<leader>h', '<C-w>s', opts) -- vertical split
-vim.keymap.set('n', '<leader>se', '<C-w>=', opts) -- vertical split
-vim.keymap.set('n', '<leader>sx', ':close <CR>', opts) -- vertical split
-
--- toggle line wrapping
-vim.keymap.set('n', '<leader>lw', '<cmd>set wrap!<CR>', opts)
-
-vim.api.nvim_set_option("clipboard", "unnamedplus")
-
-vim.api.nvim_create_autocmd('TextYankPost', {
-  desc = 'Highlight when yanking (copying) text',
-  group = vim.api.nvim_create_augroup('kickstart-highlight-yank', { clear = true }),
-  callback = function()
-    vim.highlight.on_yank()
-  end,
-})
-
-vim.keymap.set('n', '<leader>sln', function()
-   print('Showing line numbers')
-   vim.o.number = true
-   vim.o.relativenumber = true
- end, {desc= 'Show line numbers'})
-
- vim.keymap.set('n', '<leader>hln', function()
-   print('Hiding line numbers')
-   vim.o.number = false
-   vim.o.relativenumber = false
-end, {desc= 'Hide line numbers'})
+vim.schedule(function()
+  require "mappings"
+end)
