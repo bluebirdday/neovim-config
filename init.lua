1848a240f1cd699406dde2e8551138452e5894a6
Martin Dubbelman <martindubbelman@bluebirdday.nl>
2025-05-15 13:01:22 +0200

    Highlight text on yank

--- init.lua
+++ init.lua
@@ -35,3 +35,13 @@ require "nvchad.autocmds"
 vim.schedule(function()
   require "mappings"
 end)
+
+vim.api.nvim_create_autocmd('TextYankPost', {
+  desc = 'Highlight when yanking (copying) text',
+  group = vim.api.nvim_create_augroup('kickstart-highlight-yank', { clear = true }),
+  callback = function()
+    vim.highlight.on_yank()
+  end,
+})
+
+
